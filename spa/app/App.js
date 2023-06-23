407d66ec0e0af83e1d59f987a94f830b49b07627
panzer34 <116589120+panzer34@users.noreply.github.com>
2023-06-23 08:21:41 -0400

    quinto-commit

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,3 +1,13 @@
+import api from "./helpers/wp_api.js"
+import { ajax } from "./helpers/ajax.js";
+import { Header } from "./components/Header.js";
+import { Loader } from "./components/Loader.js";
+
 export function App() {
-  document.getElementById("root").innerHTML = `<h1>Bienvenidos a mi primer SPA con Vainilla JS</h1>`; 
+  const d = document,
+    $root = d.getElementById("root");
+  
+  $root.appendChild(Header());
+  $root.appendChild(Loader());
+
 }
